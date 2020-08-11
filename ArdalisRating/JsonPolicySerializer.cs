655e5472fa472d2ed2c3d5cfa9731526f77de8e0
Carlos González <carlos0202@users.noreply.github.com>
2020-08-11 17:25:25 -0400

    Initial Refactoring
    - Extracted responsibilities out of main RatingEngine class to comply with the single responsibility principle of SOLID.
    
    - changed .Net Core version to match one of the already installed versions locally.

---
+++ ArdalisRating/JsonPolicySerializer.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+
+namespace ArdalisRating
+{
+    public class JsonPolicySerializer
+    {
+        public Policy GetPolicyFromJsonString(string policyJson)
+        {
+            return JsonConvert.DeserializeObject<Policy>(
+                policyJson,
+                new StringEnumConverter());
+        }
+    }
+}
